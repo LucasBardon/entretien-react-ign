a5ab013966042ebfaa77b83a120a67fbdd499b51
Lucas Bardon <lucas.bardon@viacesi.fr>
2022-09-07 11:24:28 +0100

    Commit ex 2
    Commit exercice 2 test entretien IGN

--- src/pages/Exercice2.js
+++ src/pages/Exercice2.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Fetch from './comp2/FetchUsers.component';
 
 const API_URL = 'https://api.github.com/users';
 
 function Exercice2() {
-  return <div></div>;
+  return <div>
+    <Fetch />
+  </div>;
 }
 
 export default Exercice2;
